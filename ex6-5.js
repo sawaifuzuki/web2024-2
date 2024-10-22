b40134fddbcf3791de4cea040dcc216ae7a74ee3
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-10-22 10:03:49 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -3,6 +3,3 @@ function sayhello65(){alert('Hello,'+who65+'!');}
 function taro65(){who65='太郎';}
 function hanako65(){who65='花子';}
 
-var who66='world';
-function taro66(){alert('太郎が来た');}
-function hanako66(){alert('花子が来た');}
