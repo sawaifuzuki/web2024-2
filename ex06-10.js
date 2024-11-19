dac65d24b1f9cd9d1053051f43a74643623ee173
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-11-19 10:10:38 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('mark').src='./image/sun.png';}
-function imgchange2(){document.getElementById('mark').src='./image/rain.png';}
+function imgchange1(){document.getElementById('mark').src='./image/rain.png';}
+function imgchange2(){document.getElementById('mark').src='./image/sun.png';}
