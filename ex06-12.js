7aeee8d25e337f1b213d84b2b6119ab1c7ffb2e8
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-11-19 10:39:55 +0900

    Update ex06-12.js

--- ex06-12.js
+++ ex06-12.js
@@ -4,5 +4,4 @@ function hyoji(){//表示する文字 var str="一文字ずつ表示する。";
 if(cnt<11){//現在より１文字多く切り出して表示　document.timer.moji.value=str.substr(0,cnt+1);}
 else{//すべて表示されたら、空文字に戻す document.timer.moji.value="";}
 function startfnc(){//関数hyoji()を1000ミリ秒間隔で呼び出す setInterval("hyoji()",1000);}
-const str='Mozilla';console.log(str.substr(1,2));//expected output:"oz"
-console.log(str.substr(2));//expected output:"zilla"
+
