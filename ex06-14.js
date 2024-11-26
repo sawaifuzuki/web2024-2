c512bee36ab1703b00d056f47f08628a18201622
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-11-26 10:43:48 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,1 +1,6 @@
 function imgchange5(){document.getElementById('logo').src='./image/josailogo.jpg';}
+ <h1>練習問題6-14</h1>
+ <form name="timer14">
+ <img src='./image/josailogo.jpg' id="logo" alt="Josai Logo" class="imagechange">
+ <input type="button" value="画像切り替え" onclick="setTimeout14('imgchange5()',3000)">
+ </form>
