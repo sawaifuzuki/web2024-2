f131917212a7243114fea0b00e972f87a3d81ccf
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-03 09:57:50 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,3 +1,3 @@
 function imgchange5(){document.getElementById('logo').src='./image/josailogo.jpg';}
-function imagechange6(){
+function imagechange6(){document.getElementById('logo').src='./image/josailogo.jpg';}
  
