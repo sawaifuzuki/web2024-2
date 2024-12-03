0328a36a574a75d1155015e62c8b769b8e520d37
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-03 12:14:06 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,4 +1,3 @@
-function imgchange5(){document.getElementById('logo').src='./image/joukoma.jpg';
-                      setTimeout("imgchange6()",3000);}
-function imgchange6(){document.getElementById('logo').src='./image/josailogo.jpg';}
+function imgchange5(){document.getElementById('logo').src='./image/joukoma.jpg';}
+function mes2(){document.getElementById('logo').src='./image/josailogo.jpg';}
  
