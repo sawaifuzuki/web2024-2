b2a0b56b58fd9215c610781ec3196c88515e4563
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-09 10:21:34 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,3 +1,3 @@
 function add(){var x=document.getElementById('input1').value;
                var y=document.getElementById('input2').value;
-               alert(x+y);}
+               alert(Number(x+y));}
