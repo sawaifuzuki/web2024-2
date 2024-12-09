67d8c1731fc312485490460b39706818d7467baa
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-09 10:51:39 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,3 +1,3 @@
-function add73(){var x=document.getElementById('input731').value;
-                 var y=document.getElementById('input732').value;
-                 alert(x/y^2);}
+Nfunction add73(){var x=document.getElementById('input731').value;
+                  var y=document.getElementById('input732').value;
+                  alert(Number(x)/Number(y)^Number('2'));}
