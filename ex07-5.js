c5034cd0f1d94ae8f2c4b074f47557a2b1b6f2d3
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-10 10:03:27 +0900

    Update ex07-5.js

--- ex07-5.js
+++ ex07-5.js
@@ -1,2 +1,2 @@
-function nazo(){if(document.getElementById('answer').value=='夢'){alert('あたり!');}
+function nazo75(){if(document.getElementById('answer75').value=='夢'){alert('あたり!');}
                 else{alert('はずれ');}}
