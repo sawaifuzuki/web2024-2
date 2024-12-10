0729f5ed32fc1bf05d32403d24d8bdbf31219fc5
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-10 11:32:23 +0900

    Create ex07-10.js

---
+++ ex07-10.js
@@ -0,0 +1,3 @@
+function add710(){var c=document.getElementById('input7101').value;
+                  var d=document.getElementById('input7102').value;
+                  alert(Number(c)/(Number(d)*Number(d)));}
