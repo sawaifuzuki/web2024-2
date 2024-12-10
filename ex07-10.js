1a50b8db5c7591d42043f67d0d01b50ee4c5f61d
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-10 11:47:12 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,6 +1,6 @@
 function add710(){var c=document.getElementById('input7101').value;
                   var d=document.getElementById('input7102').value;
                   alert(Number(c)/(Number(d)*Number(d)));}
-function body710(){if(document.getElementById('add710').value<Number(18.5)){alert('低体重');}
+function body710(){if(document.getElementById('Number(add710)').value<Number(18.5)){alert('低体重');}
                   else if(document.getElementById('add710').value<Number(25)){alert('普通');}
                   else{alert('肥満');}}
