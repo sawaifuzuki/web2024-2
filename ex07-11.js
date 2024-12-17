beb184c4befcb8fb5560add510c46807fb11d13d
sawaifuzuki <167832916+sawaifuzuki@users.noreply.github.com>
2024-12-17 10:02:57 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -1,4 +1,4 @@
 function year(){var e=document.getElementById('year').value;
-                if(Number(e)%==4){alert('うるう年');}
+                if(Number(e)%4==0){alert('うるう年');}
               　if else(Number(e)%100==0){alert('平年');}
               　else {alert('平年');}}
